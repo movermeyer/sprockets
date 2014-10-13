1a19d61b5018ed1753720b1bfe0029d724c90e2f
Gavin M. Roy <gavinr@aweber.com>
2014-10-13 17:14:15 -0400

    Initial WIP cleanup
    - Add initial import of auto-loaders. @TODO add contract invocation of auto-loaders after application initialization
      - Add the ability to list out the auto-loaders
      - Stub-out flag for ignoring loaders
    - Change cli flags around to accomodate auto-loaders
    - Update error messages
    - Remove extra line feeds in error messages
    - Remove application requirement in the argparse settings
    - Abstract out iterating through package resources to single method and invoke it for auto_loaders and controllers

--- sprockets/cli.py
+++ sprockets/cli.py
@@ -70,32 +70,39 @@ class CLI(object):
     """The core Sprockets CLI application providing argument parsing and
     logic for starting a controller.
 
-    The package or module for an application is passed into
-
     """
-
+    AUTO_LOADERS = 'sprockets.auto-loader'
     CONTROLLERS = 'sprockets.controller'
 
     def __init__(self):
+        self._auto_loaders = self._get_auto_loaders()
         self._controllers = self._get_controllers()
-        self._arg_parser = argparse.ArgumentParser()
+        self.arg_parser = argparse.ArgumentParser()
         self._add_cli_args()
-        self._args = self._arg_parser.parse_args()
+        self._args = self.arg_parser.parse_args()
 
     def run(self):
         """Evaluate the command line arguments, performing the appropriate
         actions so the application can be started.
 
         """
-        # The list command prevents any other processing of args
-        if self._args.list:
+        # The apps command prevents any other processing of args
+        if self._args.apps:
+            if not self._controllers:
+                print('ERROR: No application controllers installed\n')
+                sys.exit(1)
             self._print_installed_apps(self._args.controller)
+            sys.exit(0)
+
+        # The loaders command prevents any other processing of args
+        if self._args.loaders:
+            self._print_installed_auto_loaders()
             sys.exit(0)
 
         # If app is not specified at this point, raise an error
         if not self._args.application:
-            sys.stderr.write('\nerror: application not specified\n\n')
-            self._arg_parser.print_help()
+            sys.stderr.write('ERROR: Application not specified\n')
+            self.arg_parser.print_help()
             sys.exit(-1)
 
         # If it's a registered app reference by name, get the module name
@@ -112,36 +119,42 @@ class CLI(object):
             self._controllers[self._args.controller].main(app_module,
                                                           self._args)
         except TypeError as error:
-            sys.stderr.write('error: could not start the %s controller for %s'
-                             ': %s\n\n' % (self._args.controller,
-                                           app_module,
-                                           str(error)))
+            sys.stderr.write('ERROR: could not start the %s controller for %s'
+                             ': %s\n' % (self._args.controller,
+                                         app_module,
+                                         str(error)))
             sys.exit(-1)
 
     def _add_cli_args(self):
         """Add the cli arguments to the argument parser."""
+        self.arg_parser.add_argument('-a', '--apps',
+                                     action='store_true',
+                                     help='List installed applications')
 
-        # Optional cli arguments
-        self._arg_parser.add_argument('-l', '--list',
-                                      action='store_true',
-                                      help='List installed sprockets apps')
+        self.arg_parser.add_argument('-l', '--loaders',
+                                     action='store_true',
+                                     help='List installed auto-loaders')
 
-        self._arg_parser.add_argument('-s', '--syslog',
-                                      action='store_true',
-                                      help='Log to syslog')
+        self.arg_parser.add_argument('-n', '--no-loaders',
+                                     action='store_true',
+                                     help='Disable Auto-Loader modules')
 
-        self._arg_parser.add_argument('-v', '--verbose',
-                                      action='count',
-                                      help=('Verbose logging output, use -vv '
-                                            'for DEBUG level logging'))
+        self.arg_parser.add_argument('-s', '--syslog',
+                                     action='store_true',
+                                     help='Log to syslog')
 
-        self._arg_parser.add_argument('--version',
-                                      action='version',
-                                      version='sprockets v%s ' % __version__)
+        self.arg_parser.add_argument('-v', '--verbose',
+                                     action='count',
+                                     help=('Verbose logging output, use -vv '
+                                           'for DEBUG level logging'))
+
+        self.arg_parser.add_argument('--version',
+                                     action='version',
+                                     version='sprockets v%s ' % __version__)
 
         # Controller sub-parser
-        subparsers = self._arg_parser.add_subparsers(dest='controller',
-                                                          help=DESCRIPTION)
+        subparsers = self.arg_parser.add_subparsers(dest='controller',
+                                                    help=DESCRIPTION)
 
         # Iterate through the controllers and add their cli arguments
         for key in self._controllers:
@@ -153,9 +166,10 @@ class CLI(object):
                 LOGGER.debug('%s missing add_cli_arguments()', key)
 
         # The application argument
-        self._arg_parser.add_argument('application',
-                                      action="store",
-                                      help='The sprockets app to run')
+        self.arg_parser.add_argument('application',
+                                     default=None, nargs="?",
+                                     action="store",
+                                     help='The sprockets app to run')
 
     @staticmethod
     def _configure_logging(application, verbosity=0, syslog=False):
@@ -222,18 +236,25 @@ class CLI(object):
         """
         return argparse.ArgumentParser()
 
-    def _get_controllers(self):
-        """Iterate through the installed controller entry points and import
-        the module and assign the handle to the CLI._controllers dict.
+    def _get_auto_loaders(self):
+        """Iterate through the installed auto loader entry points and import
+        the modules, returning the dict to be assigned to the CLI._auto_loaders
+        dict.
 
         :return: dict
 
         """
-        controllers = dict()
-        for pkg in pkg_resources.iter_entry_points(group=self.CONTROLLERS):
-            LOGGER.debug('Loading %s controller', pkg.name)
-            controllers[pkg.name] = importlib.import_module(pkg.module_name)
-        return controllers
+        return self._get_package_resources(self.AUTO_LOADERS)
+
+    def _get_controllers(self):
+        """Iterate through the installed controller entry points and import
+        the modules, returning the dict to be assigned to the CLI._controllers
+        dict.
+
+        :return: dict
+
+        """
+        return self._get_package_resources(self.CONTROLLERS)
 
     def _get_controller_help(self, controller):
         """Return the value of the HELP attribute for a controller that should
@@ -246,16 +267,44 @@ class CLI(object):
             return self._controllers[controller].HELP
         return None
 
+    @staticmethod
+    def _get_package_resources(group):
+        """Iterate through the installed entry points for the specified group,
+        importing each package, returning a dict of handles by package name.
+
+        :return: dict
+
+        """
+        packages = dict()
+        for pkg in pkg_resources.iter_entry_points(group=group):
+            packages[pkg.name] = importlib.import_module(pkg.module_name)
+        return packages
+
     def _print_installed_apps(self, controller):
         """Print out a list of installed sprockets applications
 
         :param str controller: The name of the controller to get apps for
+
         """
-        print('\nInstalled Sprockets %s Apps\n' % controller.upper())
+        print('Installed Sprockets {0} Apps\n'.format(controller.upper()))
         print("{0:<25} {1:>25}".format('Name', 'Module'))
         print(string.ljust('', 51, '-'))
         for app in self._get_applications(controller):
-            print('{0:<25} {1:>25}'.format(app.name, '(%s)' % app.module_name))
+            print('{0:<25} {1:>25}'.format(app.name,
+                                           '({0})'.format(app.module_name)))
+        print('')
+
+    def _print_installed_auto_loaders(self):
+        """Print out a list of installed auto-loader packages"""
+        if not self._auto_loaders:
+            print('There are no auto-loader packages installed\n')
+            return
+        print('Installed Sprockets Auto-Loaders\n')
+        print("{0:<25} {1:>25}".format('Name', 'Module'))
+        print(string.ljust('', 51, '-'))
+        for loader in self._auto_loaders:
+            print('{0:<25} {1:>25}'.format(loader.name,
+                                           '({0})'.format(loader.module_name)))
         print('')
 
 
